0e0af82682dfc716f83bad96157a228e4eeb331a
Astapkina Ekaterina <33911738+AEkaterina@users.noreply.github.com>
2021-06-07 16:49:52 +0300

    done

---
+++ lab01.ru/a.php
@@ -0,0 +1,4 @@
+<?php
+header ("Content-Type: text/html; charset=utf-8");
+    echo date('l jS \of F Y h:i:s A');
+?>
---
+++ lab01.ru/delay.php
@@ -0,0 +1,5 @@
+<?php
+header ("Content-Type: text/html; charset=utf-8");
+    sleep(3);
+    echo date('l jS \of F Y h:i:s A');
+?>
